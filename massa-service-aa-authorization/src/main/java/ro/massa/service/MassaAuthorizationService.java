a312d164ba2ef4e919c9c2295403ff2a9572327a
George-Andrei Dima <dima331453@gmail.com>
2021-10-30 12:52:40 +0300

    removed useless path in authrization controller
    Signed-off-by: George-Andrei Dima <dima331453@gmail.com>

--- massa-service-aa-authorization/src/main/java/ro/massa/service/MassaAuthorizationService.java
+++ massa-service-aa-authorization/src/main/java/ro/massa/service/MassaAuthorizationService.java
@@ -5,7 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface MassaAuthorizationService {
 
-    byte[] verifyAuthorizationCertificateRequest(byte[] authorizationRequest);
-
-    String resolveAuthorizationCertificateRequest();
+    byte[] resolveAuthorizationCertificateRequest(byte[] authorizationRequest);
 }
