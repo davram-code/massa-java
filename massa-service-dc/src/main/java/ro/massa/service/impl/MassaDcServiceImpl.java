d87556497d37be500a3f352868ca6c6a1048739f
George-Andrei Dima <dima331453@gmail.com>
2021-12-10 00:56:23 +0200

    issue #14: added DC service for exposing CRL/CTL
    Signed-off-by: George-Andrei Dima <dima331453@gmail.com>

---
+++ massa-service-dc/src/main/java/ro/massa/service/impl/MassaDcServiceImpl.java
@@ -0,0 +1,46 @@
+package ro.massa.service.impl;
+
+
+import org.springframework.stereotype.Component;
+import ro.massa.common.MassaLog;
+import ro.massa.common.MassaLogFactory;
+import ro.massa.service.MassaDcService;
+
+import java.io.File;
+import java.nio.file.Files;
+
+@Component
+public class MassaDcServiceImpl implements MassaDcService {
+    MassaLog log = MassaLogFactory.getLog(MassaDcServiceImpl.class);
+
+    private static byte[] getByteArray(String fileName) throws Exception {
+        File fin = new File(fileName);
+        return Files.readAllBytes(fin.toPath());
+    }
+
+    @Override
+    public byte[] getCTL() {
+        try{
+            return getByteArray("certificates/ctl.bin");
+        }
+        catch (Exception e)
+        {
+            log.error(e.getMessage());
+            return  null;
+        }
+    }
+
+    @Override
+    public byte[] getCRL() {
+        try{
+            return getByteArray("certificates/crl.bin");
+        }
+        catch (Exception e)
+        {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+
+}
