d9034afbc3ffba2ac8b5dbd5e42caed031eca68e
davram-code <cos.avram@gmail.com>
2022-12-07 17:05:53 +0200

    modify build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,21 @@
 set -e
 
 alias cp="cp -v"
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.15.0.10-1.fc35.x86_64"
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk"
 
 cd CryptoClient
 ./gradlew jar -Dorg.gradle.java.home=/usr/lib/jvm/java-11
 cd ..
 
-
+mkdir -p c2c-common/libs
 cp -v CryptoClient/build/libs/CryptoClient-1.0-SNAPSHOT-plain.jar c2c-common/libs
 
 cd c2c-common
 ./gradlew jar -Dorg.gradle.java.home=/usr/lib/jvm/java-11
 cd ..
 
+mkdir -p massa-lib/libs/
+mkdir -p massa-cli/libs/
 cp -v c2c-common/build/libs/c2c-common.jar massa-lib/libs/c2c-common.main.jar
 cp -v c2c-common/build/libs/c2c-common.jar massa-cli/libs/c2c-common.main.jar
 cp -v CryptoClient/build/libs/CryptoClient-1.0-SNAPSHOT-plain.jar massa-lib/libs
@@ -24,6 +26,7 @@ cp -v CryptoClient/build/libs/CryptoClient-1.0-SNAPSHOT-plain.jar massa-lib/libs
 cd massa-lib
 ./gradlew jar -Dorg.gradle.java.home=/usr/lib/jvm/java-11
 cd ..
+mkdir -p massa-cli/libs
 cp -v massa-lib/build/libs/massa-lib-1.0-SNAPSHOT.jar massa-cli/libs/massa-lib.main.jar
 
 cd massa-cli
@@ -31,5 +34,6 @@ cd massa-cli
 cd ..
 
 
+mkdir -p massa-cli/libs/
 cp -v massa-cli/build/libs/massa-cli-1.0-SNAPSHOT.jar massa-cli/libs/
 cp -v CryptoClient/build/libs/CryptoClient-1.0-SNAPSHOT-plain.jar massa-cli/libs/
